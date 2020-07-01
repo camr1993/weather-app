8f4ca7e8f613dca4381e103721ff10e74734d68d
Cameron Ratliff <cratliff@colgate.edu>
2020-07-01 16:28:59 -0400

    weather api

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Test from './Test'
 import LocationForm from './LocationForm'
+import axios from 'axios'
 
-const Routes = () => {
-  return (
-    <Switch>
-      <Route path="/test" component={Test} />
-      <Route component={LocationForm} />
-    </Switch>
-  )
+export default class Routes extends Component {
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e8551b988fd858314363ae664d8bdb9c`
+      )
+      console.log('DATA:', data)
+    } catch (error) {
+      console.log(`Get Weather Error: ${error}`)
+    }
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route path="/test" component={Test} />
+        <Route component={LocationForm} />
+      </Switch>
+    )
+  }
 }
-
-export default Routes
 
 // WITHROUTER???
