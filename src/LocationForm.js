fd5084185655c618497bca12d24784399a2c97ac
Cameron Ratliff <cratliff@colgate.edu>
2020-07-01 14:59:34 -0400

    location form base

--- src/LocationForm.js
+++ src/LocationForm.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
 
 export default class LocationForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      location: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    // do something here
+    this.setState({
+      location: '',
+    })
+  }
+
   render() {
-    return <div>Location Form</div>
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="location">Location:</label>
+        <input
+          type="text"
+          name="location"
+          required
+          value={this.state.location}
+          onChange={this.handleChange}
+        />
+      </form>
+    )
   }
 }
