5bceeb135f526c00bc01b86ac80d268f5dea4edb
Cameron Ratliff <cratliff@colgate.edu>
2020-07-02 21:29:42 -0400

    removing google api

--- src/Routes.js
+++ src/Routes.js
@@ -2,19 +2,14 @@ import React from 'react'
 import { withRouter, Route, Switch } from 'react-router-dom'
 import WeatherPage from './WeatherPage'
 import LocationForm from './LocationForm'
-import LocationForm2 from './LocationForm2'
-import LocationForm3 from './LocationForm3'
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/weatherpage" component={WeatherPage} />
-      <Route path="/test" component={LocationForm2} />
-      <Route path="/test2" component={LocationForm3} />
       <Route component={LocationForm} />
     </Switch>
   )
 }
 
 export default withRouter(Routes)
-// WITHROUTER???
